be1a3e1ffe01c571dfd9f313eb142fdc8481d342
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-09-24 22:32:27 +0900

    day3
    .

--- day3/assignment1/lib/main.dart
+++ day3/assignment1/lib/main.dart
@@ -1,125 +1,157 @@
+import 'package:assignment1/MusicTile.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      theme: ThemeData.from(colorScheme: ColorScheme.dark()),
+      home: Scaffold(
+        appBar: AppBar(
+          shadowColor: Colors.white,
+          leading: Icon(Icons.keyboard_arrow_down),
+          title: Text('아워리스트'),
+          elevation: 0,
+          shape: Border(bottom: BorderSide(color: Colors.white12, width: 1)),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.airplay),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.search),
             ),
           ],
         ),
+        body: ListView(children: [
+          MusicTile(
+            image: 'assets/images/music_come_with_me.png',
+            title: 'Come with me',
+            subTitle: 'Surfaces 및 salem ilese',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'Good day',
+            subTitle: 'Surfaces',
+            image: 'assets/images/music_good_day.png',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'Honesty',
+            subTitle: 'Pink Sweat\$',
+            image: 'assets/images/music_honesty.png',
+            time: '3:09',
+          ),
+          MusicTile(
+            title: 'I Wish I Missed My Ex',
+            subTitle: '마할리아 버크마',
+            image: 'assets/images/music_i_wish_i_missed_my_ex.png',
+            time: '3:24',
+          ),
+          MusicTile(
+            title: 'Plastic Plants',
+            subTitle: '마할리아 버크마',
+            image: 'assets/images/music_plastic_plants.png',
+            time: '3:20',
+          ),
+          MusicTile(
+            title: 'Sucker for you',
+            subTitle: '맷 테리',
+            image: 'assets/images/music_sucker_for_you.png',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'Summer is for falling in love',
+            subTitle: 'Sarah Kang & Eye Love Brandon',
+            image: 'assets/images/music_summer_is_for_falling_in_love.png',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'These days(feat. Jess Glynne, Macklemore & Dan Caplen)',
+            subTitle: 'Rudimental',
+            image: 'assets/images/music_these_days.png',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'You Make Me',
+            subTitle: 'DAY6',
+            image: 'assets/images/music_you_make_me.png',
+            time: '3:00',
+          ),
+          MusicTile(
+            title: 'Zombie Pop',
+            subTitle: 'DRP IAN',
+            image: 'assets/images/music_zombie_pop.png',
+            time: '3:00',
+          ),
+        ]),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.white,
+          currentIndex: 2,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(Icons.home),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '둘러보기'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.library_music), label: '보관함')
+          ],
+        ),
+        bottomSheet: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 64,
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(4)),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Image.asset(
+                      'assets/images/music_you_make_me.png',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text('You Make Me'), Text('Day6')],
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.play_arrow),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.skip_next),
+                ),
+              ]),
+              decoration: BoxDecoration(color: Colors.white12),
+            ),
+            Container(
+              height: 1,
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: 16,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
