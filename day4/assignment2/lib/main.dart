6e4c1808fe9a8b4fa3273e38e60e6589bbf4009f
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-09-24 23:01:22 +0900

    day4/assignment2
    .

--- day4/assignment2/lib/main.dart
+++ day4/assignment2/lib/main.dart
@@ -1,157 +1,132 @@
-import 'package:assignment1/MusicTile.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // root Widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.from(colorScheme: ColorScheme.dark()),
-      home: Scaffold(
-        appBar: AppBar(
-          shadowColor: Colors.white,
-          leading: Icon(Icons.keyboard_arrow_down),
-          title: Text('아워리스트'),
-          elevation: 0,
-          shape: Border(bottom: BorderSide(color: Colors.white12, width: 1)),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.airplay),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.search),
-            ),
-          ],
-        ),
-        body: ListView(children: [
-          MusicTile(
-            image: 'assets/images/music_come_with_me.png',
-            title: 'Come with me',
-            subTitle: 'Surfaces 및 salem ilese',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'Good day',
-            subTitle: 'Surfaces',
-            image: 'assets/images/music_good_day.png',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'Honesty',
-            subTitle: 'Pink Sweat\$',
-            image: 'assets/images/music_honesty.png',
-            time: '3:09',
-          ),
-          MusicTile(
-            title: 'I Wish I Missed My Ex',
-            subTitle: '마할리아 버크마',
-            image: 'assets/images/music_i_wish_i_missed_my_ex.png',
-            time: '3:24',
-          ),
-          MusicTile(
-            title: 'Plastic Plants',
-            subTitle: '마할리아 버크마',
-            image: 'assets/images/music_plastic_plants.png',
-            time: '3:20',
-          ),
-          MusicTile(
-            title: 'Sucker for you',
-            subTitle: '맷 테리',
-            image: 'assets/images/music_sucker_for_you.png',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'Summer is for falling in love',
-            subTitle: 'Sarah Kang & Eye Love Brandon',
-            image: 'assets/images/music_summer_is_for_falling_in_love.png',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'These days(feat. Jess Glynne, Macklemore & Dan Caplen)',
-            subTitle: 'Rudimental',
-            image: 'assets/images/music_these_days.png',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'You Make Me',
-            subTitle: 'DAY6',
-            image: 'assets/images/music_you_make_me.png',
-            time: '3:00',
-          ),
-          MusicTile(
-            title: 'Zombie Pop',
-            subTitle: 'DRP IAN',
-            image: 'assets/images/music_zombie_pop.png',
-            time: '3:00',
-          ),
-        ]),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.white,
-          currentIndex: 2,
-          items: [
-            BottomNavigationBarItem(
-              backgroundColor: Colors.white,
-              icon: Icon(Icons.home),
-              label: '홈',
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: '둘러보기'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.library_music), label: '보관함')
-          ],
-        ),
-        bottomSheet: Column(
-          mainAxisSize: MainAxisSize.min,
+      home: MyHomePage(title: '분식왕 이테디 주문하기'), // 홈 페이지 호출(title 매개변수로 전달)
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title; //앱 제목
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> order = []; //주문 리스트
+
+  //음식 리스트
+  final List<Map> foodList = [
+    {'imgUrl': 'assets/option_bokki.png', 'name': '떡볶이'},
+    {'imgUrl': 'assets/option_beer.png', 'name': '맥주'},
+    {'imgUrl': 'assets/option_kimbap.png', 'name': '김밥'},
+    {'imgUrl': 'assets/option_omurice.png', 'name': '오므라이스'},
+    {'imgUrl': 'assets/option_pork_cutlets.png', 'name': '돈까스'},
+    {'imgUrl': 'assets/option_ramen.png', 'name': '라면'},
+    {'imgUrl': 'assets/option_udon.png', 'name': '우동'}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //앱바
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true, //타이틀 글씨 가운데 정렬
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 64,
-              child: Row(children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(4)),
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    child: Image.asset(
-                      'assets/images/music_you_make_me.png',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text('You Make Me'), Text('Day6')],
-                  ),
-                ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.play_arrow),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.skip_next),
-                ),
-              ]),
-              decoration: BoxDecoration(color: Colors.white12),
-            ),
-            Container(
-              height: 1,
-              alignment: Alignment.centerLeft,
-              child: Container(
-                width: 16,
-                color: Colors.white,
+            Text(
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
-            )
+              '주문 리스트',
+            ),
+            Text(order.toString()),
+            SizedBox(height: 8),
+            Text(
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              '음식',
+            ),
+            Expanded(
+              // 음식 리스트 그리드 뷰
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(foodList.length, (index) {
+                  return GestureDetector(
+                    onTap: () => setState(() {
+                      order.add(foodList[index]['name']);
+                    }),
+                    // 하나의 음식 요소
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Image(
+                              image: AssetImage(
+                                foodList[index]['imgUrl'],
+                              ),
+                              height: MediaQuery.of(context).size.height,
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Text(
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            foodList[index]['name'].toString(),
+                          ),
+                          Text(
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            '[담기]',
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
           ],
         ),
       ),
+      // 하단 버튼
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => setState(() {
+          order.clear();
+        }),
+        label: Text('초기화하기'),
+      ),
+      // 하단 버튼 위치
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
