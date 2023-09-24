d28c92f591e892032468703ac02090882447c139
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-09-24 11:51:51 +0900

    day2
    .

--- day2/assignment2/lib/main.dart
+++ day2/assignment2/lib/main.dart
@@ -11,74 +11,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body:SafeArea(
-              child:Column(
+        home: Scaffold(
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ListTile(
-                    title:Text('이테디'),
-                    subtitle: Text('010-1000-2000'),
-                    trailing : Icon(Icons.call),
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        shape:BoxShape.circle,
-                      ),
-                      clipBehavior : Clip.antiAlias,
-                      child:Image.network('https://picsum.photos/100/100'),
+                  Text('오늘의 하루는', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                  Text('어땠나요?', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+
+                  Container(
+                    width: 200,
+                    height: 100,
+                    child: PageView(
+                        children : [
+                          SizedBox.expand(
+                            child: Container(
+                              child: Center(
+                                  child: Text(
+                                    '행복', style: TextStyle(fontFamily: 'Sunflower', color: Colors.white, fontSize: 23),
+                                  )
+                              ),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Colors.orange,
+                                    Colors.yellow,
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox.expand(
+                              child: Container(
+                                child: Center(
+                                    child: Text(
+                                      '우울', style: TextStyle(fontFamily: 'Sunflower', color: Colors.white, fontSize: 23),
+                                    )
+                                ),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Colors.black,
+                                      Colors.white,
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                          SizedBox.expand(
+                              child: Container(
+                                child: Center(
+                                    child: Text(
+                                      '지금 지쳤나요?', style: TextStyle(fontFamily: 'Sunflower', color: Colors.white, fontSize: 23),
+                                    )
+                                ),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Colors.blue,
+                                      Colors.green,
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                        ]
                     ),
-                  ),
-                  ListTile(
-                    title:Text('김스틴'),
-                    subtitle: Text('010-9000-8000'),
-                    trailing : Icon(Icons.call),
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        shape:BoxShape.circle,
-                      ),
-                      clipBehavior : Clip.antiAlias,
-                      child:Image.network('https://picsum.photos/100/100'),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
                     ),
+                    clipBehavior: Clip.antiAlias,
                   ),
-                  ListTile(
-                    title:Text('이주노'),
-                    subtitle: Text('010-3000-3000'),
-                    trailing : Icon(Icons.call),
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        shape:BoxShape.circle,
-                      ),
-                      clipBehavior : Clip.antiAlias,
-                      child:Image.network('https://picsum.photos/100/100'),
+
+                  Divider(),
+
+                  Container(
+                    height: 80,
+                    decoration: BoxDecoration(color: Colors.blue ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: CircleAvatar(
+                              radius: 28,
+                              backgroundImage: NetworkImage('https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/678/25404daf10e3d1b6926e86dcbc3463b8_res.jpeg')
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('한예지', style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.w400),),
+                              Text('AI개발', style: TextStyle(color: Colors.white, fontSize: 12),),
+                              Text('python, Pycharm', style: TextStyle(color: Colors.white, fontSize: 12),),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                            child: Icon(Icons.add,
+                                   color:Colors.white,
+                            ),
+                        ),
+                      ],
                     ),
-                  ),
-                  ListTile(
-                    title:Text('임헬렌'),
-                    subtitle: Text('010-2000-8000'),
-                    trailing : Icon(Icons.call),
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        shape:BoxShape.circle,
-                      ),
-                      clipBehavior : Clip.antiAlias,
-                      child:Image.network('https://picsum.photos/100/100'),
-                    ),
-                  ),
-                  ListTile(
-                    title:Text('염해리'),
-                    subtitle: Text('010-1000-1000'),
-                    trailing : Icon(Icons.call),
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        shape:BoxShape.circle,
-                      ),
-                      clipBehavior : Clip.antiAlias,
-                      child:Image.network('https://picsum.photos/100/100'),
-                    ),
-                  ),
-                ],
-              )
-          )
-      ),
+                  )
+                ]
+            )
+        )
     );
   }
 }
