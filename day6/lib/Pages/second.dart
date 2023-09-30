c22280cb7457c38e3f12208c6e4f3afb2bfc72c0
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-09-30 23:50:01 +0900

    day6/assignment1/second
    python zzang

--- day6/lib/Pages/second.dart
+++ day6/lib/Pages/second.dart
@@ -4,34 +4,50 @@ import 'package:day6/data.dart';
 class Second extends StatelessWidget{
   const Second({super.key});
 
-  Widget listview_builder(){
-    return ListView.builder(
-        scrollDirection: Axis.vertical,
-        padding : EdgeInsets.all(5),
-        itemCount: mathData.length,
-        itemBuilder: (BuildContext context, int index){
-          return Container(
-            color:Colors.amber,
-            child:Center(
-              child: Text("${mathData[index]}"),
-            ),
-          );
-        }
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar:AppBar(title:Text('2번과제')),
+        body:Container(
+          child: listview(),
+        )
     );
   }
+}
+
+class listview extends StatelessWidget{
+  const listview({super.key});
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-        title:'2번과제',
-        home:Scaffold(
-            appBar:AppBar(title:Text('2번과제')),
-            body:Container(
-                child: Expanded(
-                  child: listview_builder(),
-                )
-            )
-        )
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: mathData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: EdgeInsets.all(5),
+              width: (mathData[index] * 2).toDouble(),
+              height: 24.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(90),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), //x축:오른쪽,y축:아래
+                    )
+                  ]
+              ),
+              child: Center(
+                child: Text("${mathData[index]}"),
+              ),
+            ),
+          );
+        }
     );
   }
 }
